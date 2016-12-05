39387906000bc67dedce8e3bef7026e8dc70c251
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2016-12-05 16:16:11 +0900

    Fix error

--- bucketsort/bucketsort.exs
+++ bucketsort/bucketsort.exs
@@ -1,32 +1,43 @@
 defmodule BucketSort.Scheduler do
   @name {:global, __MODULE__}
-  def start_link(list) when is_list(list), do: Agent.start_link(fn -> {list, []} end, name: @name)
+  def start_link(list) when is_list(list), do: Agent.start_link(fn -> {list, [], []} end, name: @name)
 
-  def get_list, do: Agent.get(@name, fn {list, _} -> list end)
+  def get_list, do: Agent.get(@name, fn {list, _, _} -> list end)
 
-  def get_clients, do: Agent.get(@name, fn {_, clients} -> clients end)
+  def get_clients, do: Agent.get(@name, fn {_, clients, _} -> clients end)
 
-  def add_clients(client), do: Agent.update(@name, fn {list, clients} -> {list, [client|clients]} end)
+  def get_answers, do: Agent.get(@name, fn {_, _, answers} -> answers end)
+
+  def add_client(client), do: Agent.update(@name, fn {list, clients, answers} -> {list, [client|clients], answers} end)
+
+  def add_answer(answer), do: Agent.update(@name, fn {list, clients, answers} -> {list, clients, [answer|answers]} end)
+
+  defp delete_client(client), do: Agent.update(@name, fn {list, clients, answers} -> {list, List.delete(clients, client), answers} end)
 
   def run do
     clients = get_clients
     send_ready(clients)
-    split_list(get_list, clients) |> schedule_processes(length(clients))
+    split_bucket(get_list, clients) |> schedule_processes(length(clients))
   end
 
-  def send_ready(clients) do
+  defp send_ready(clients) do
     clients |> Enum.each(&(send &1, {:ready, self}))
   end
 
-  defp split_list(list, clients) do
-    length(list) / length(clients)
+  defp split_bucket(list, clients) do
+    min = Enum.min(list)
+    max = Enum.max(list)
+    Float.ceil((max - min) / length(clients))
   end
 
   defp schedule_processes(split, 0) do
     receive do
       {:ready, client} ->
         send client, {:shutdown}
-        schedule_processes(split, 0)
+        delete_client(client)
+        if length(get_clients) > 0 do
+          schedule_processes(split, 0)
+        end
     end
   end
 
@@ -37,43 +48,50 @@ defmodule BucketSort.Scheduler do
         schedule_processes(split, n-1)
     end
   end
+
+  def print_result(answers) do
+    sorted_list = Enum.reduce(Enum.sort(answers, fn {n1, _}, {n2, _} -> n1 <= n2 end), [], fn({_, list}, acc) -> acc ++ list end)
+    IO.puts(inspect(sorted_list))
+  end
 end
 
 defmodule BucketSort.Client do
   def run do
     task = Task.async(&wait_receive_ready/0)
-    Fib.Scheduler.add_clients(task.pid)
+    BucketSort.Scheduler.add_client(task.pid)
   end
 
-  def wait_receive_ready do
+  defp wait_receive_ready do
     receive do
       { :ready, scheduler } ->
-        IO.puts("ready")
         receiver(scheduler)
       { :shutdown } ->
         exit(:normal)
     end
   end
 
-  def receiver(scheduler) do
+  defp receiver(scheduler) do
     send scheduler, {:ready, self}
     receive do
-      { :bucket, split, n} ->
-        min = n * split
-        max = (n+1) * split
-        bsort(BucketSort.Scheduler.get_list, min, max)
+      { :bucket, split, bn} ->
+        list= BucketSort.Scheduler.get_list 
+        min = Enum.min(list) + (bn-1) * split
+        max = Enum.min(list) + bn * split
+        BucketSort.Scheduler.add_answer({bn, bsort(list, min, max, bn)})
         receiver(scheduler)
       { :shutdown } ->
         exit(:normal)
     end
   end
 
-  def bsort(list, min, max) do
-    bucket = Enum.filter(list, &(inbucket?(&1, min, max)))
+  defp bsort(list, min, max, bn) do
+    bucket = Enum.filter(list, &(in_bucket?(&1, min, max, bn)))
     Enum.sort(bucket)
   end
 
-  def inbucket?(n, min, max) when min <= n and n < max, do: true
+  defp in_bucket?(n, _, max, 1) when n <= max, do: true
 
-  def inbucket?(_, _, _), do: false
+  defp in_bucket?(n, min, max, _) when min < n and n <= max, do: true
+
+  defp in_bucket?(_, _, _, _), do: false
 end
