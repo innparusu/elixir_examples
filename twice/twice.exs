742ae87daac90659197900133443fcf03c68f7e4
innparusu95 <e125716@ie.u-ryukyu.ac.jp>
2016-12-10 00:44:42 +0900

    Change list to range for twice.exs

--- twice/twice.exs
+++ twice/twice.exs
@@ -1,32 +1,36 @@
 defmodule Twice.Scheduler do
   @name {:global, __MODULE__}
-  def start_link(list) when is_list(list), do: Agent.start_link(fn -> {list, [], []} end, name: @name)
+  def start_link(list) do: Agent.start_link(fn -> {list, [], []} end, name: @name)
 
-  def get_list, do: Agent.get(@name, fn {list, _, _} -> list end)
+  def get_range, do: Agent.get(@name, fn {range, _, _} -> range end)
 
   def get_clients, do: Agent.get(@name, fn {_, clients, _} -> clients end)
 
   def get_answers, do: Agent.get(@name, fn {_, _, answers} -> answers end)
 
-  def add_client(client), do: Agent.update(@name, fn {list, clients, answers} -> {list, [client|clients], answers} end)
+  def add_client(client), do: Agent.update(@name, fn {range, clients, answers} -> {range, [client|clients], answers} end)
 
-  def add_answer(answer), do: Agent.update(@name, fn {list, clients, answers} -> {list, clients, [answer|answers]} end)
+  def add_answer(answer), do: Agent.update(@name, fn {range, clients, answers} -> {range, clients, [answer|answers]} end)
 
-  defp delete_client(client), do: Agent.update(@name, fn {list, clients, answers} -> {list, List.delete(clients, client), answers} end)
+  defp delete_client(client), do: Agent.update(@name, fn {range, clients, answers} -> {range, List.delete(clients, client), answers} end)
+
+  def connect_nodes(stream), do: Enum.each(stream, &:connect_node/1)
+
+  defp connect_node(node), do: String.strip(node) |> String.to_aton |> Node.connect
 
   def run do
     clients = get_clients
-    list = get_list
+    range = get_range
     send_ready(clients)
-    split_clients(list, clients) |> schedule_processes(length(clients), list)
+    split_clients(range, clients) |> schedule_processes(length(clients), range)
   end
 
   defp send_ready(clients) do
     clients |> Enum.each(&(send &1, {:ready, self}))
   end
 
-  defp split_clients(list, clients) do
-    Float.ceil(length(list)/length(clients))
+  defp split_clients(s..e, clients) do
+    Float.ceil(e/length(clients))
   end
 
   defp schedule_processes(_, 0, _) do
@@ -40,12 +44,12 @@ defmodule Twice.Scheduler do
     end
   end
 
-  defp schedule_processes(split, n, list) do
+  defp schedule_processes(split, n, range) do
     receive do
       {:ready, client} ->
-        split_list = Enum.slice(list, round((n-1) * split), round(split))
+        split_list = Enum.slice(range, round((n-1) * split), round(split))
         send client, {:twice, n, split_list}
-        schedule_processes(split, n-1, list)
+        schedule_processes(split, n-1, range)
     end
   end
 
